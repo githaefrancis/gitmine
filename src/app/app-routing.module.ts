de5bcada11626109e4b4883175e78d960c835327
Francis Githae <francis.githae@student.moringaschool.com>
2022-01-15 14:20:59 +0300

    update single user view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SearchPageComponent } from './search-page/search-page.component';
+import { SingleUserComponent } from './single-user/single-user.component';
 
 const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'search',component:SearchPageComponent},
   {path:'search/:query',component:SearchPageComponent},
+  {path:'user',component:SingleUserComponent},
   {path:'',redirectTo:'/home',pathMatch:'full'}
 ];
 
