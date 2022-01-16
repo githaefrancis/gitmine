95effce28f6e97ecdd9edcd0df6d93d986b3918d
Francis Githae <francis.githae@student.moringaschool.com>
2022-01-16 23:04:20 +0300

    apply time elapsed pipe for date of user creation

--- src/app/time-elapsed-pipe/time-elapsed.pipe.ts
+++ src/app/time-elapsed-pipe/time-elapsed.pipe.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
+import * as moment from 'moment';
 @Pipe({
-  name: 'timeElapsed'
+  name: 'timeElapsed',
+  pure:false
 })
 export class TimeElapsedPipe implements PipeTransform {
 
-  transform(value:any): unknown {
-    return null;
+  transform(value:any): any {
+    var timeElapsed=moment(value).fromNow();
+    return timeElapsed;
   }
 
 }
